781c5b2d38497f42209e2dd6c1a08bf44d7e0aec
amutarbo-dev <amutarbo.dev@gmail.com>
2023-08-09 20:27:08 +0200

    feat: First aproximation to home screen with products

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, retry, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  urlProducts = `${environment.backUrl}/product`;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getProducts(limit: number, skip: number) {
+    const urlProducts = `${this.urlProducts}?limit=${limit}&skip=${skip}`;
+    return this.http
+      .get<any>(urlProducts)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
+  }
 }
