b2500a94ebb18f2dc0448ebdc463ca5334b29f98
amutarbo-dev <amutarbo.dev@gmail.com>
2023-08-15 19:36:18 +0200

    feat: register

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { AuthService } from '@services/auth.service';
 
 @Component({
@@ -16,7 +17,8 @@ export class RegisterPage {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private alertController: AlertController
   ) {
     this.userForm = this.fb.group(
       {
@@ -54,10 +56,29 @@ export class RegisterPage {
       this.authService
         .registerUser(this.userForm.value)
         .subscribe((res: any) => {
-          this.router.navigate(['/home']);
+          this.presentAlert({
+            header: 'Registro completado',
+            message: 'Inicie sesion para continuar',
+          });
         });
     } catch (err: any) {
       // Modal Error Programar
     }
+  }
+
+  async presentAlert(content: { header: string; message: string }) {
+    const alert = await this.alertController.create({
+      header: content.header,
+      message: content.message,
+      buttons: [
+        {
+          text: 'Ok',
+          role: 'confirm',
+          handler: () => this.router.navigate(['/login']),
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
