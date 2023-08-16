0f40ce13283a702138a2ac0827bf49c9cc31de9f
amutarbo-dev <amutarbo.dev@gmail.com>
2023-08-16 23:38:05 +0200

    feat: Login integration with access guards

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '@services/local-storage.service';
 import { ProductsService } from 'src/app/shared/services/products.service';
 
 @Component({
@@ -13,25 +15,37 @@ export class HomePage implements OnInit {
     { name: 'Items', path: '/items', icon: 'pricetag-outline' },
     { name: 'Contact', path: '/contact', icon: 'chatbubble-outline' },
     { name: 'About us', path: '/aboutus', icon: 'mail-outline' },
-    { name: 'Log out', path: '/logout', icon: 'mail-outline' },
+    {
+      name: 'Log out',
+      path: '/logout',
+      icon: 'exit-outline',
+      action: () => {
+        this.localStorage.removeAll();
+        this.router.navigate(['/login']);
+      },
+    },
   ];
 
   listOfProducts: any;
 
-  constructor(private productService: ProductsService) {}
+  constructor(
+    private productService: ProductsService,
+    private localStorage: LocalStorageService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    // this.productService.getProducts(20, 0).subscribe((res) => {
-    //   this.listOfProducts = res.map((item: any) => {
-    //     const idPhoto = item._id.replace(
-    //       /(\d{3})(\d{3})(\d{3})(\d{4})/,
-    //       '$1/$2/$3/$4'
-    //     );
-    //     return {
-    //       ...item,
-    //       idPhoto,
-    //     };
-    //   });
-    // });
+    this.productService.getProducts(20, 0).subscribe((res) => {
+      this.listOfProducts = res.map((item: any) => {
+        const idPhoto = item._id.replace(
+          /(\d{3})(\d{3})(\d{3})(\d{4})/,
+          '$1/$2/$3/$4'
+        );
+        return {
+          ...item,
+          idPhoto,
+        };
+      });
+    });
   }
 }
